afc28c4d47ad2817ebe1d07aecf9e3c7beb1cf5c
Samyukthasrir <123433350+Samyukthasrir@users.noreply.github.com>
2024-06-22 08:53:37 +0530

    Add files via upload

---
+++ even r odd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"enter a number";
+	cin>>n;
+	if(n%2==0){
+		cout<<"it is even number";
+	}
+	else{
+		cout<<"it is odd number";
+	}
+	return 0;
+}
