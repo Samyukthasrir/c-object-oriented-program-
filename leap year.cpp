7db405a380e77505e9122c5dbc5f0f6e907c7847
Samyukthasrir <123433350+Samyukthasrir@users.noreply.github.com>
2024-06-22 08:55:57 +0530

    Add files via upload

--- leap year.cpp
+++ leap year.cpp
@@ -1,20 +1,20 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main() {
-  int year;
-  cout << "Enter a year: ";
-  cin >> year;
-  if (year % 400 == 0) {
-    cout << year << " is a leap year.";
-  }
-  else if (year % 100 == 0) {
-    cout << year << " is not a leap year.";
-  }
-  else if (year % 4 == 0) {
-    cout << year << " is a leap year.";
-  }
-  else {
-    cout << year << " is not a leap year.";
-  }
-  return 0;
+int main(){
+	int year;
+	cout<<"enter a year";
+	cin>>year;
+	if(year%4==0){
+		cout<<"it is a leap year";
+	}
+	else if(year%400==0){
+		cout<<"it is a leap year";
+	}
+	else if(year%100==0){
+		cout<<"it is not a leap year";
+	}
+	else{
+		cout<<"error";
+	}
+	return 0;
 }
