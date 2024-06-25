5ce04ca226f67844d92c162d4a68f893df4545e0
Samyukthasrir <123433350+Samyukthasrir@users.noreply.github.com>
2024-06-25 09:49:37 +0530

    Add files via upload

---
+++ multi table using for loop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    int number;
+
+    std::cout << "Enter a number to print its multiplication table: ";
+    std::cin >> number;
+
+    std::cout << "Multiplication table for " << number << ":\n";
+
+    for (int i = 1; i <= 10; ++i) {
+        std::cout << number << " * " << i << " = " << number * i << std::endl;
+    }
+
+    return 0;
+}
+
